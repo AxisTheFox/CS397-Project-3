f3edc82dee84ff61c30b19811d0e7ca9c99fce54
Braydon Fox <foxden00@gmail.com>
2017-03-25 16:16:16 -0400

    Added data parsing and html formatting/display.

--- Forecast/Forecast.aspx
+++ Forecast/Forecast.aspx
@@ -4,7 +4,8 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Five Day Forecasts</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
     <form id="form1" runat="server">
--- Forecast/Forecast.aspx.cs
+++ Forecast/Forecast.aspx.cs
@@ -17,6 +17,8 @@ namespace Forecast
 
         protected void submitButton_Click(object sender, EventArgs e)
         {
+            forecasts.InnerHtml = "";
+
             string zipCodes = zipCodeTextBox.Text;
 
             WeatherService.ndfdXML weatherService = new WeatherService.ndfdXML();
@@ -30,11 +32,105 @@ namespace Forecast
             XmlDocument forecastsXmlDocument = new XmlDocument();
             forecastsXmlDocument.LoadXml(forecastsXml);
 
-            forecasts.InnerHtml += "<table>";
-            XmlNodeList locations = forecastsXmlDocument.GetElementsByTagName("parameters");
-            foreach(XmlNode location in locations)
-            {
 
+            XmlNodeList locations = forecastsXmlDocument.GetElementsByTagName("parameters");
+            foreach (XmlNode location in locations)
+            {
+                XmlDocument locationXml = new XmlDocument();
+                locationXml.LoadXml(location.OuterXml);
+
+                List<double> maxTemperatures = new List<double>();
+                List<double> minTemperatures = new List<double>();
+                XmlNodeList temperatureNodes = locationXml.GetElementsByTagName("temperature");
+                foreach (XmlNode temperatureNode in temperatureNodes)
+                {
+                    if (temperatureNode.Attributes["type"].Value.Equals("maximum"))
+                    {
+                        foreach (XmlNode maxTemperature in temperatureNode.ChildNodes)
+                        {
+                            if (maxTemperature.Name.Equals("value"))
+                            {
+                                maxTemperatures.Add(double.Parse(maxTemperature.InnerText));
+                            }
+                        }
+                    }
+                    if (temperatureNode.Attributes["type"].Value.Equals("minimum"))
+                    {
+                        foreach (XmlNode minTemperature in temperatureNode.ChildNodes)
+                        {
+                            if (minTemperature.Name.Equals("value"))
+                            {
+                                minTemperatures.Add(double.Parse(minTemperature.InnerText));
+                            }
+                        }
+                    }
+                }
+
+                List<string> conditionIconUrls = new List<string>();
+                XmlNodeList conditionIconUrlNodes = locationXml.GetElementsByTagName("conditions-icon");
+                foreach (XmlNode conditionIconUrl in conditionIconUrlNodes)
+                {
+                    foreach (XmlNode iconUrlNode in conditionIconUrl.ChildNodes)
+                    {
+                        if (iconUrlNode.Name.Equals("icon-link"))
+                        {
+                            conditionIconUrls.Add(iconUrlNode.InnerText);
+                        }
+                    }
+                }
+
+                List<string> descriptions = new List<string>();
+                XmlNodeList weatherDescriptionNodes = locationXml.GetElementsByTagName("weather");
+                foreach (XmlNode weatherDescriptions in weatherDescriptionNodes)
+                {
+                    foreach (XmlNode descriptionNode in weatherDescriptions.ChildNodes)
+                    {
+                        if (descriptionNode.Name.Equals("weather-conditions"))
+                        {
+                            descriptions.Add(descriptionNode.Attributes["weather-summary"].InnerText);
+                        }
+                    }
+                }
+
+                forecasts.InnerHtml += "<p><table>";
+
+                forecasts.InnerHtml += "<tr>";
+                foreach(double maxTemp in maxTemperatures)
+                {
+                    forecasts.InnerHtml += "<td>";
+                    forecasts.InnerHtml += "High: " +  maxTemp;
+                    forecasts.InnerHtml += "</td>";
+                }
+                forecasts.InnerHtml += "</tr>";
+
+                forecasts.InnerHtml += "<tr>";
+                foreach (string iconUrl in conditionIconUrls)
+                {
+                    forecasts.InnerHtml += "<td>";
+                    forecasts.InnerHtml += "<img src=" + iconUrl + ">";
+                    forecasts.InnerHtml += "</td>";
+                }
+                forecasts.InnerHtml += "</tr>";
+
+                forecasts.InnerHtml += "<tr>";
+                foreach (double minTemp in minTemperatures)
+                {
+                    forecasts.InnerHtml += "<td>";
+                    forecasts.InnerHtml += "Low: " + minTemp;
+                    forecasts.InnerHtml += "</td>";
+                }
+                forecasts.InnerHtml += "</tr>";
+
+                forecasts.InnerHtml += "<tr>";
+                foreach (string weatherDescription in descriptions)
+                {
+                    forecasts.InnerHtml += "<td>";
+                    forecasts.InnerHtml += weatherDescription;
+                    forecasts.InnerHtml += "</td>";
+                }
+                forecasts.InnerHtml += "</tr>";
+
+                forecasts.InnerHtml += "</table></p>";
             }
         }
     }
